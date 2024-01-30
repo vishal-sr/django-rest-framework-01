f47b240a3e381627821c2396df7657edd25d4a0b
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-01-30 22:08:49 +0530

    Model 'Student' is created and migrated to 'studentsdb' database

--- students/models.py
+++ students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    score = models.DecimalField(max_digits=2, decimal_places=2, validators=[
+        MinValueValidator(0), MaxValueValidator(100)
+    ])
