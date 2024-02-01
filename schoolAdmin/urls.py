838f7916e5e3075397cdceceee17555d103bb36a
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-01 22:23:14 +0530

    Small changes

--- schoolAdmin/urls.py
+++ schoolAdmin/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("students.urls"))
+    path("students/", include("students.urls"))
 ]
--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.all_students)
+    path("", views.all_students),
+    path("<int:id>", views.one_student)
 ]
