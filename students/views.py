5423d8c461c96bb66fd30a054f9e030d83de5de3
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-02 14:27:12 +0530

    Implemented mixins for 'AllStudents' view class

--- students/views.py
+++ students/views.py
@@ -4,24 +4,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import mixins, generics
 
 from .models import Student
 from .serializers import StudentSerializer
 # Create your views here.
 
 
-class AllStudents(APIView):
+class AllStudents(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
     def get(self, request):
-        modelData = Student.objects.all()
-        serializer = StudentSerializer(modelData, many=True)
-        return Response(serializer.data)
+        return self.list(request)
 
     def post(self, request):
-        serializer = StudentSerializer(request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
 
 class OneStudent(APIView):
