59a27c8a4d68f5d0674e141d4de11f1f5a2464fb
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-03 22:48:57 +0530

    Removed un-used package imports

--- students/views.py
+++ students/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework import mixins, generics
 
 from .models import Student
