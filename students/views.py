67c8a02ce5a8eac6f33f0621cdc77c974281bebc
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-07 22:15:10 +0530

    Implemented limitoffsetpagination

--- students/views.py
+++ students/views.py
@@ -1,14 +1,10 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.pagination import PageNumberPagination
-
-
-class StudentPagination(PageNumberPagination):
-    page_size = 2
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    pagination_class = StudentPagination
+    pagination_class = LimitOffsetPagination
