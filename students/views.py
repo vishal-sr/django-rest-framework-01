284d7e58245cad6ec341997f8ef09400adbc31bf
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-07 22:04:01 +0530

    Implated same pagination in class level in views.py

--- students/views.py
+++ students/views.py
@@ -1,8 +1,14 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+
+
+class StudentPagination(PageNumberPagination):
+    page_size = 2
 
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    pagination_class = StudentPagination
