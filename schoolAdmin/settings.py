ad9f88c3b9f2bc6dee9aba7e6876824bd828f8c7
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-02-09 14:07:48 +0530

    Implemented filters

--- schoolAdmin/settings.py
+++ schoolAdmin/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "students",
-    "rest_framework"
+    "rest_framework",
+    "django_filters"
 ]
 
 REST_FRAMEWORK = {
--- students/views.py
+++ students/views.py
@@ -2,9 +2,12 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'score']
